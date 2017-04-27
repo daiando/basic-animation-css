560de525aa8c0303db7f4126194ba7bc70ed99e3
daiando <dai.ando@rakuten.com>
2017-04-27 15:30:21 +0900

    finish course

---
+++ src/app/new-project/animation.ts
@@ -0,0 +1,27 @@
+import { trigger, state, style, transition, animate, keyframes, group } from '@angular/animations';
+
+export const buttonStateTrigger = trigger('buttonState', [
+  state('valid', style({
+    backgroundColor: 'lightgreen',
+    borderColor: 'green',
+    color: 'green'
+  })),
+  state('invalid', style({
+    backgroundColor: 'red',
+    borderColor: 'darkred',
+    color: 'white'
+  })),
+  transition('invalid => valid', [
+    group([
+      animate(100, style({
+        transform: 'scale(1.1)'
+      })),
+      animate(200, style({
+        backgroundColor: 'red'
+      }))
+    ]),
+    animate(200, style({
+      transform: 'scale(1)'
+    }))
+  ])
+]);
---
+++ src/app/shared/route-animations.ts
@@ -0,0 +1,27 @@
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
+export const routeFadeStateTrigger = trigger('routeFadeState', [
+  transition(':enter', [
+    style({
+      opacity: 0
+    }),
+    animate(500)
+  ]),
+  transition(':leave', animate(500, style({
+    opacity: 0
+  })))
+]);
+
+export const routeSlideStateTrigger= trigger('routeSlideState', [
+  transition(':enter', [
+    style({
+      transform: 'translateY(-100%)',
+      opacity: 0
+    }),
+    animate(500)
+  ]),
+  transition(':leave', animate(500, style({
+    transform: 'translateY(100%)',
+    opacity: 0
+  })))
+]);
